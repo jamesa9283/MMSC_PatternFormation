5d627ee620df0e150836a0051fb7dd1790f26b0f
James Arthur <jamesarthur7@icloud.com>
2023-02-03 14:14:57 +0000

    code works (?), at least it looks kind of correct.

--- fd.py
+++ fd.py
@@ -2,15 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Parameters
-M = 5
 a = 0.1
 b = 0.9
-Nx = 40
-Nt = 100
-gamma = 0.1
+Nx = 100
+Nt = 50
+gamma = 0.5
 d = 1
 L = 1
-T = 0.06
+T = 1
 
 
 x = np.linspace(0, L, Nx+1) # mesh points in space
@@ -21,36 +20,45 @@ dt = t[1] - t[0]
 mu = dt / dx**2
 
 u = np.zeros((Nx+1, Nt+1)) # unknown u
+v = np.zeros((Nx+1, Nt+1)) # unknown v
 
-def f(u, x, t):
-    return 0
+def f(u, v):
+    return a - u + u**2*v 
+
+def g(u, v):
+    return b - u**2*v
 
 def I(x):
-    return 0.1
+    return 1
 
-# Set initial condition u(x,0) = I(x)
+def J(x):
+    return 1
+
+# Set initial condition u(x,0) = I(x) / v(x, 0) = J(x)
 for i in range(0, Nx+1):
     u[i, 0] = I(x[i])
+    v[i, 0] = J(x[i])
     
 for m in range(0, Nt-1):
     # Insert boundary conditions
-    u[0,m] = 0; u[Nx,m]=0
+    u[0,m] = 0; u[Nx,m]= 0
+    v[0, m] = 0; v[Nx, m] = 0
     
     # Compute u at inner mesh points
     for j in range(1, Nx):
-        u[j, m+1] = u[j,m] + mu*(u[j-1,m] - 2*u[j,m] + u[j+1,m]) + gamma*f(u[j,m], x[j], t[m])
+        u[j, m+1] = u[j,m] + mu*(u[j-1,m] - 2*u[j,m] + u[j+1,m]) + dt*gamma*f(u[j,m], v[j,m]) 
+        # forever let it be known, Luna forgot that it was dt * gamma * f
+        v[j, m+1] = v[j,m] + d*mu*(u[j-1,m] - 2*u[j,m] + u[j+1,m]) + dt*gamma*g(u[j,m], v[j,m])
         print(u[j,m])
             
 # print(u)
 
 # plot
 fig = plt.figure()
-ax = plt.axes(projection='3d')
-
-X, T = np.meshgrid(x, t)
-ax.plot_surface(T, X, u.T, linewidth=2.0)
-
-fig = plt.figure()
-ax2 = plt.axes()
-ax2.plot(X, T)
+plt.imshow(u, interpolation='nearest')
 plt.show()
+
+# fig = plt.figure()
+# ax2 = plt.axes()
+# ax2.plot(X, T)
+# plt.show()
